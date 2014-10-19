b1d2db7e2308a2c7c0cd4ace011390f494edf03b
aferlazzo <aferlazzo@github.com>
2014-10-19 14:46:34 -0700

    addinglessons

---
+++ unit/1/madlibs/madlibs.js
@@ -0,0 +1,16 @@
+/**
+ * Created by anthony on 10/18/14.
+ */
+angular.module('myApp', []);
+
+angular.module('myApp').controller('myController', function ($scope) {
+	$scope.female_name = "______________";
+	$scope.job_title = "______________";
+	$scope.tedious_task = "______________";
+	$scope.dirty_task = "______________";
+	$scope.celebrity = "______________";
+	$scope.obnoxious_celebrity = "______________";
+	$scope.adjective = "______________";
+	$scope.useless_skill = "______________";
+	$scope.huge_number = "______________";
+});
